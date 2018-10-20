89ddcbf98f081b20efcd9fe4c0de8d279936aa9b
Christopher Magee <frostydknight@gmail.com>
2018-10-20 10:29:31 -0500

    jquery files

--- js/DragBox.js
+++ js/DragBox.js
@@ -1,57 +1,62 @@
 // Setup the dnd listeners.
-var dropZone = document.getElementById('drop_zone');
-dropZone.addEventListener('dragover', handleDragOver, false);
-dropZone.addEventListener('drop', handleFileSelect, false);
-dropZone.addEventListener('dragleave',handleDragLeave,false);
+$(document).ready(function() {
+  var dropZone = document.getElementById('drop_zone');
+  dropZone.addEventListener('dragover', handleDragOver, false);
+  dropZone.addEventListener('drop', handleFileSelect, false);
+  dropZone.addEventListener('dragleave',handleDragLeave,false);
 
-function handleFileSelect(evt) {
-    evt.stopPropagation();
-    evt.preventDefault();
+  var pageReady;
 
-    var files = evt.dataTransfer.files; // FileList object.
-    const f = files[0];
-    document.getElementById("drop_zone").innerHTML = f.type+"  ";
+  function handleFileSelect(evt) {
+      evt.stopPropagation();
+      evt.preventDefault();
 
-    if(f.type.match("image/*")) {
-        document.getElementById("drop_zone").innerHTML = "<h3 id='drag-text'>Drag and drop image here!</h3>";
-        document.getElementById("drag-text").innerHTML = "That's an image alright.";
-        encodeImageFileAsURL(f);
-    }
-    else {
-        document.getElementById("drop_zone").innerHTML = "<h3 id='drag-text'>Drag and drop image here!</h3>";
-        document.getElementById("drag-text").innerHTML = "Sorry, that's not an image. Please try again.";
+      var files = evt.dataTransfer.files;
+      const f = files[0];
+      document.getElementById("drop_zone").innerHTML = f.type+"  ";
+
+      if(f.type.match("image/*")) {
+          document.getElementById("drop_zone").innerHTML = "<h3 id='drag-text'>Drag and drop image here!</h3>";
+          document.getElementById("drag-text").innerHTML = "That's an image alright.";
+          encodeImageFileAsURL(f);
       }
-    // files is a FileList of File objects. List some properties.
-
-    /*output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
-    f.size, ' bytes, last modified: ',
-    f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
-    '</li>');
-    document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';*/
-}
-function encodeImageFileAsURL(f) {
-
-  var filesSelected = f;
-  var fileToLoad = f;
-  var fileReader = new FileReader();
-  fileReader.onload = function(fileLoadedEvent) {
-    var srcData = fileLoadedEvent.target.result; // <--- data: base64
-    console.log(srcData);
-    console.log("Converted Base64 version is " + srcData);
+      else {
+          document.getElementById("drop_zone").innerHTML = "<h3 id='drag-text'>Drag and drop image here!</h3>";
+          document.getElementById("drag-text").innerHTML = "Sorry, that's not an image. Please try again.";
+        }
   }
-  fileReader.readAsDataURL(fileToLoad);
-}
+  function encodeImageFileAsURL(f) {
 
-function handleDragOver(evt) {
-    document.getElementById("drag-text").innerHTML = "Release file within this box.";
-    evt.stopPropagation();
-    evt.preventDefault();
-    evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
-}
+    var filesSelected = f;
+    var fileToLoad = f;
+    var fileReader = new FileReader();
+    fileReader.onload = function(fileLoadedEvent) {
+      var srcData = fileLoadedEvent.target.result; // <--- data: base64
+      //uploadEncodedImage(srcData);
+    }
+    fileReader.readAsDataURL(fileToLoad);
+    changePage();
+  }
 
-function handleDragLeave(evt) {
-    document.getElementById("drag-text").innerHTML = "Drag and drop image here!";
-    evt.stopPropagation();
-    evt.preventDefault();
-    evt.dataTransfer.dropEffect = '';
-}
+  function handleDragOver(evt) {
+      document.getElementById("drag-text").innerHTML = "Release file within this box.";
+      evt.stopPropagation();
+      evt.preventDefault();
+      evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+  }
+
+  function handleDragLeave(evt) {
+      document.getElementById("drag-text").innerHTML = "Drag and drop image here!";
+      evt.stopPropagation();
+      evt.preventDefault();
+      evt.dataTransfer.dropEffect = '';
+  }
+
+  function changePage() {
+    $("#drop_zone").remove();
+    $("#form-group").remove();
+    $("#Home").append("<div class='border border-width-8 col-sm-3' id='image-container'></div>");
+    $("#Home").append("<div class='border border-width-8 col-sm-3' id='emoji-container'></div>");
+  }
+
+});
