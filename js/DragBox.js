2959e46b5cad2b15f5576e2805125199d868ce88
Christopher Magee <frostydknight@gmail.com>
2018-10-20 09:07:00 -0500

    what I'm working on

--- js/DragBox.js
+++ js/DragBox.js
@@ -13,11 +13,13 @@ function handleFileSelect(evt) {
     document.getElementById("drop_zone").innerHTML = f.type+"  ";
 
     if(f.type.match("image/*")) {
-        document.getElementById("drop_zone").innerHTML += "That's an image alright.";
+        document.getElementById("drop_zone").innerHTML = "<h3 id='drag-text'>Drag and drop image here!</h3>";
+        document.getElementById("drag-text").innerHTML = "That's an image alright.";
     }
-    else
-        document.getElementById("drop_zone").innerHTML += "Nope, not an image at all!";
-
+    else {
+        document.getElementById("drop_zone").innerHTML = "<h3 id='drag-text'>Drag and drop image here!</h3>";
+        document.getElementById("drag-text").innerHTML = "Nope, not an image at all!";
+      }
     // files is a FileList of File objects. List some properties.
 
     /*output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
@@ -26,18 +28,38 @@ function handleFileSelect(evt) {
     '</li>');
     document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';*/
 }
+function encodeImageFileAsURL(f) {
+
+  var filesSelected = f;
+  if (filesSelected.length > 0) {
+    var fileToLoad = filesSelected[0];
+
+    var fileReader = new FileReader();
+
+    fileReader.onload = function(fileLoadedEvent) {
+      var srcData = fileLoadedEvent.target.result; // <--- data: base64
+
+      var newImage = document.createElement('img');
+      newImage.src = srcData;
+
+      document.getElementById("imgTest").innerHTML = newImage.outerHTML;
+      alert("Converted Base64 version is " + document.getElementById("imgTest").innerHTML);
+      console.log("Converted Base64 version is " + document.getElementById("imgTest").innerHTML);
+    }
+    fileReader.readAsDataURL(fileToLoad);
+  }
+}
 
 function handleDragOver(evt) {
-    document.getElementById("drop_zone").innerHTML = "Release file within this box.";
+    document.getElementById("drag-text").innerHTML = "Release file within this box.";
     evt.stopPropagation();
     evt.preventDefault();
     evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
 }
 
 function handleDragLeave(evt) {
-    document.getElementById("drop_zone").innerHTML = "Drag and drop an image to generate tags for!";
+    document.getElementById("drag-text").innerHTML = "Drag and drop image here!";
     evt.stopPropagation();
     evt.preventDefault();
     evt.dataTransfer.dropEffect = '';
 }
-
