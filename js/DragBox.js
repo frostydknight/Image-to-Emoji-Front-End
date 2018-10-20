4dd9e40f2eee6ad04ddf471e37a66564039612e7
Tom Yarrow <tyarrow1@gmail.com>
2018-10-20 11:23:31 -0400

    added image uploading (untested)

--- js/DragBox.js
+++ js/DragBox.js
@@ -2,7 +2,7 @@
 var dropZone = document.getElementById('drop_zone');
 dropZone.addEventListener('dragover', handleDragOver, false);
 dropZone.addEventListener('drop', handleFileSelect, false);
-dropZone.addEventListener('dragleave',handleDragLeave,false);
+dropZone.addEventListener('dragleave', handleDragLeave, false);
 
 function handleFileSelect(evt) {
     evt.stopPropagation();
@@ -10,9 +10,9 @@ function handleFileSelect(evt) {
 
     var files = evt.dataTransfer.files; // FileList object.
     const f = files[0];
-    document.getElementById("drop_zone").innerHTML = f.type+"  ";
+    document.getElementById("drop_zone").innerHTML = f.type + "  ";
 
-    if(f.type.match("image/*")) {
+    if (f.type.match("image/*")) {
         document.getElementById("drop_zone").innerHTML = "<h3 id='drag-text'>Drag and drop image here!</h3>";
         document.getElementById("drag-text").innerHTML = "That's an image alright.";
         encodeImageFileAsURL(f);
@@ -20,7 +20,7 @@ function handleFileSelect(evt) {
     else {
         document.getElementById("drop_zone").innerHTML = "<h3 id='drag-text'>Drag and drop image here!</h3>";
         document.getElementById("drag-text").innerHTML = "Sorry, that's not an image. Please try again.";
-      }
+    }
     // files is a FileList of File objects. List some properties.
 
     /*output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
@@ -29,17 +29,35 @@ function handleFileSelect(evt) {
     '</li>');
     document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';*/
 }
+
 function encodeImageFileAsURL(f) {
 
-  var filesSelected = f;
-  var fileToLoad = filesSelected[0];
-  var fileReader = new FileReader();
-  fileReader.onload = function(fileLoadedEvent) {
-    var srcData = fileLoadedEvent.target.result; // <--- data: base64
-    console.log(srcData);
-    console.log("Converted Base64 version is " + srcData);
-  }
-  fileReader.readAsDataURL(fileToLoad);
+    var filesSelected = f;
+    var fileToLoad = filesSelected[0];
+    var fileReader = new FileReader();
+    fileReader.onload = function (fileLoadedEvent) {
+        var srcData = fileLoadedEvent.target.result; // <--- data: base64
+        console.log(srcData);
+        console.log("Converted Base64 version is " + srcData);
+        uploadEncodedImage(srcData);
+    };
+    fileReader.readAsDataURL(fileToLoad);
+}
+
+function uploadEncodedImage(s) {
+    var url = "localhost:5000/";
+    var http = new XMLHttpRequest();
+    http.open("POST", url);
+    http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    http.onreadystatechange = function () {
+        if(http.readyState === 3 && http.status === 200) {
+            document.getElementById("drag-text").innerHTML = "Loading...";
+        }
+        if(http.readyState === 4 && http.status === 200) {
+            console.log(http.responseText);
+        }
+    };
+    http.send(s);
 }
 
 function handleDragOver(evt) {
